d0aa72e52b35dac0f32c9f9f5fe98f20d0bdf290
Jamil Appa <jamil.appa@zenotech.com>
2018-12-12 20:02:51 +0000

    Updates

--- develop/_static/documentation_options.js
+++ develop/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v2018.09.62-686-gb540',
+    VERSION: 'v2018.09.62-693-g1d0bf',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
