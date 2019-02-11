236a3feefa1d3c570694dde9ba1628fd9bd9458a
Oliver Darbyshire <oliver.darbyshire@zenotech.com>
2019-02-11 15:22:16 +0000

    Update tutorial section of the user guide

--- tutorial/_static/documentation_options.js
+++ tutorial/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v2019.01.68-7-g8aa64',
+    VERSION: 'v2019.01.68-277-g6cd5',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
