7ab3b0f33167041794d41e5a88f5551ade398607
Zenotech <support@zenotech.com>
2019-06-13 14:46:48 +0000

    Adding userguide for branch master

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v2019.05.85',
+    VERSION: 'v2019.06.86',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
