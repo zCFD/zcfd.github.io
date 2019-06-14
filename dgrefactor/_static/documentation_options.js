35a96790f78a0021837aa85639640f5aeb1074b0
Zenotech <support@zenotech.com>
2019-06-14 16:23:49 +0000

    Adding userguide for branch dgrefactor

--- dgrefactor/_static/documentation_options.js
+++ dgrefactor/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v2019.06.118-dgrefactor',
+    VERSION: 'v2019.06.119-dgrefactor',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
