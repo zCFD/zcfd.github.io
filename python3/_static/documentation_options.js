cdc92cd99aa1576bf53903f4e3a7d607c1fbb066
Zenotech <support@zenotech.com>
2020-02-04 12:40:19 +0000

    Adding userguide for branch python3

--- python3/_static/documentation_options.js
+++ python3/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v2020.02.32-python3',
+    VERSION: 'v2020.02.34-python3',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
