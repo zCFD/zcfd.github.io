b2e75bbe46efbd78a3a4cdb6253b9456997ab22f
Zenotech <support@zenotech.com>
2020-02-18 16:32:58 +0000

    Adding userguide for branch nastran-reader

--- nastran-reader/_static/documentation_options.js
+++ nastran-reader/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v2020.02.4-nastran-reader',
+    VERSION: 'v2020.02.6-nastran-reader',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
