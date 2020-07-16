ff5b102c5d1690ef5c27bd8438e4276d38c55475
Zenotech <support@zenotech.com>
2020-07-16 13:31:47 +0100

    Adding userguide for branch bazel

--- bazel/_static/documentation_options.js
+++ bazel/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v2020.07.11-bazel',
+    VERSION: 'v2020.07.12-bazel',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
