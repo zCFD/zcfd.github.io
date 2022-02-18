a3e9b2623356f9ad42ba4ec9e19dd475c0c5c34b
Zenotech CI <support@zenotech.com>
2022-02-18 08:52:03 +0000

    Adding userguide for branch buildkite_release

--- buildkite_release/_static/documentation_options.js
+++ buildkite_release/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v2021.11.764-231-ga79f4',
+    VERSION: 'v2021.11.764-233-g49d47',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
