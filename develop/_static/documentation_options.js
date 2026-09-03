dfc23e6f79faed11c9321f351bfdb9470baf076e
Zenotech CI <support@zenotech.com>
2026-09-03 09:38:10 +0000

    Adding userguide for branch develop

--- develop/_static/documentation_options.js
+++ develop/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: 'v2025.11.13331-286-gf58d5',
+    VERSION: 'v2025.11.13331-287-gf6da7',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
